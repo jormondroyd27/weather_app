c6935fb8a1ce5d4f9159ab65db046284a3082ddd
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2022-05-27 19:41:09 +0100

    Created Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+# This python file is where we will create the Flask Application and make calls to the weather API.
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# map this application to a route
+@app.route('/')
+def index():
+    return "Chuckin' it down!!!!"
