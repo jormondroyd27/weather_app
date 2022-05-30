4e4dab640272af8b23064439cb4809769d09cb28
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2022-05-30 16:21:04 +0100

    added more data from the api call to use

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-# This python file is where we will create the Flask Application and make calls to the weather API.
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, url_for, redirect
+from api import get_weather
+import requests, os
 
 app = Flask(__name__)
 
-# map this application to a route
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/city', methods=['POST'])
+def post_weather():
+    city = request.form['city']
+    data = get_weather(city)
+    temp = data['main']['temp']
+    min = data['main']['temp_min']
+    max = data['main']['temp_max']
+    description = data['weather'][0]['main']
+    location = data['name']
+    country = data['sys']['country']
+    return render_template('results.html', city=data, temp=temp, description=description, location=location, country=country, min=min, max=max)
+
+# ensures the app only runs once and multiple instances are not created
+if __name__ == '__main__':
+    app.run()
